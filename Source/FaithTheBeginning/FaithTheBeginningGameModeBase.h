cf43e9bbbefd640bf3bab69c0b778ed1fd947409
filipw01 <filipw01@interia.eu>
2017-03-09 17:03:08 +0100

    Project from the beginning

---
+++ Source/FaithTheBeginning/FaithTheBeginningGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "FaithTheBeginningGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FAITHTHEBEGINNING_API AFaithTheBeginningGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
