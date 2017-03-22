e591fc82467a94041f1926f1113239debbf0dd61
filipw01 <filipw01@interia.eu>
2017-03-22 22:10:44 +0100

    First Changes in Unity
    Unity is awesome

---
+++ Assets/NewBehaviourScript1.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime;
+        transform.Translate(x, 0, 0);
+	}
+}
