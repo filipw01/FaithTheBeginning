e2ab3bfa700d6d8d8a2f71e398b8edf0a14456a4
filipw01 <filipw01@interia.eu>
2017-03-25 01:28:36 +0100

    Letting main character moving and jumping

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetTouch(0).phase == TouchPhase.Stationary)
+        {
+            transform.Translate(1 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            transform.Translate(-1 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetTouch(1).phase == TouchPhase.Began)
+        {
+            rb.AddForce(new Vector3(0, 10, 0), ForceMode2D.Impulse);
+        }
+    }
 }
